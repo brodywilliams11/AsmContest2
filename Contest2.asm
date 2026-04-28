551d0189de4f296652ee94c06943e50f6d93de97
Brody <bwilliams2023@my.fit.edu>
2026-04-27 22:43:14 -0400

    Prints current balance. Added the option to exit the game

--- Contest2.asm
+++ Contest2.asm
@@ -2,6 +2,8 @@ INCLUDE Irvine32.inc
 
 .data
 rulesMsg BYTE "Press SPACE to spin ($10) or E to exit ",0
+balanceMsg BYTE "Current Balance: $",0
+balance DWORD 100
 
 .code
 main PROC
@@ -10,6 +12,19 @@ main PROC
     call WriteString
     call Crlf
     call Crlf
+
+waitKey: 
+	mov edx, OFFSET balanceMsg
+	call WriteString
+	mov eax, balance
+	call WriteDec
+	call Crlf
+
+readInput: 
+	call ReadKey
+	cmp al, 'e'
+	je exitGame
+	jmp readInput
 
 exitGame:
     exit
